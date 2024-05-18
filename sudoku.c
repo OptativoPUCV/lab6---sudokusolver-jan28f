a32e9c7f0bac6e35d2fa54afebe705f8c63b3acb
Your Name <you@example.com>
2024-05-18 23:23:44 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -107,7 +107,7 @@ List *get_adj_nodes(Node *n)
                   Node *adjunto = copy(n);
                   adjunto->sudo[i][k] = j;
                   //print_node(adj);
-                  //if (is_valid(adjunto))
+                  if (is_valid(adjunto))
                      pushBack(list, adjunto);
                }
          }
